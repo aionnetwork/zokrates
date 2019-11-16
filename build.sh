a826fe617a029f0d2db321ee6834b7774a388ce7
ali-sharif <ali@aion.network>
2019-11-16 17:26:44 -0500

    update build file to include libsnark

--- build.sh
+++ build.sh
@@ -3,12 +3,5 @@
 # Exit if any subcommand fails
 set -e
 
-# Disable building with libsnark in this script, until PGHR13 and GM17 verifier generation is implemented.  
+cargo +nightly -Z package-features build --package zokrates_cli --features="libsnark"
 
- if [ -n "$WITH_LIBSNARK" ]; then
-	echo "building zokrates with libsnark"
-	cargo +nightly -Z package-features build --package zokrates_cli --features="libsnark"
- else
- 	echo "building zokrates without libsnark"
- 	cargo +nightly build
- fi
--- build_release.sh
+++ build_release.sh
@@ -3,12 +3,4 @@
 # Exit if any subcommand fails
 set -e
 
-# Disable building with libsnark in this script, until PGHR13 and GM17 verifier generation is implemented.  
-
-#if [ -n "$WITH_LIBSNARK" ]; then
-#	echo "building zokrates with libsnark"
-#	cargo +nightly -Z package-features build --release --package zokrates_cli --features="libsnark"
-#else
-	echo "building zokrates (release) without libsnark"
-	cargo +nightly build --release
-#fi
+cargo +nightly -Z package-features build --release --package zokrates_cli --features="libsnark"
